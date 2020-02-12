fa1f442787219ca5094e37bf95f6c117523f1a79
Abdul Khan <akhan17@outlook.com>
2020-02-12 22:44:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+array2 = []
+
+def my_each(array)
+  array2=[]
+  count = 0
+  while count != array.length
+    array2.push(yield(array[count]))
+    count +=1
+  end
+  array
 end
